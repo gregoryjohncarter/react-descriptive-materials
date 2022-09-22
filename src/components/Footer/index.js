d59b4a2db84e0a2cce796dded9fc65ba87d30464
gregoryjohncarter <gregoryjohncarter@gmail.com>
2022-09-22 15:46:45 -0400

    responsive view adjustments

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,16 @@ import { SocialIcon } from 'react-social-icons';
 
 function Footer() {
   return (
-    <footer className="marginBot mobileFooter">
-      <SocialIcon url="https://www.linkedin.com/in/gregoryjohncarter/" className="iconHover" target="_blank"/>
-      <SocialIcon url="https://github.com/gregoryjohncarter" className="iconHover" bgColor="#b1da6f" target="_blank"/>
-      <SocialIcon url="https://stackoverflow.com/users/18726394/kurama52" className="iconHover" target="_blank"/>
-    </footer>
+    <>
+      <footer className="marginBot">
+        <SocialIcon url="https://www.linkedin.com/in/gregoryjohncarter/" className="iconHover" target="_blank"/>
+        <SocialIcon url="https://github.com/gregoryjohncarter" className="iconHover" bgColor="#b1da6f" target="_blank"/>
+        <SocialIcon url="https://stackoverflow.com/users/18726394/kurama52" className="iconHover" target="_blank"/>
+      </footer>
+      <div className="marginTop">
+
+      </div>
+    </>
   );
 }
 
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -14,12 +14,12 @@ function Project() {
     <div className="border">
     <div className="main-container">
       <div className="row">
-        <div className="col s12 m5 l6 colFlex">
+        <div className="col s12 l6 colFlex">
           <div className="secondary screen-title" onClick={(event) => { handleClick('https://yks2728.github.io/Screen-Title-Scanner/', event); }} style={{cursor:"pointer"}}> 
             <SocialIcon url='https://github.com/yks2728/Screen-Title-Scanner' className="iconHover" bgColor="#e0d4ec" fgColor="#2d2d2d" style={{ height: 45, width: 45}} target="_blank"/><p className="fontTwoA" onClick={(event) => { handleClick('https://yks2728.github.io/Screen-Title-Scanner/', event); }}>Screen Title Scanner <span className="fontOneA">&rarr; jQuery, imdb API</span></p>
           </div>
         </div>
-        <div className="col s12 m5 l6 colFlex">
+        <div className="col s12 l6 colFlex">
           <div className="secondary weather" onClick={(event) => { handleClick('https://gregoryjohncarter.github.io/weather-informer-index/', event); }} style={{cursor:"pointer"}}> 
             <SocialIcon url='https://github.com/gregoryjohncarter/weather-informer-index' className="iconHover" bgColor="#e0d4ec" fgColor="#2d2d2d" style={{ height: 45, width: 45}} target="_blank"/><p className="fontTwoA" onClick={(event) => { handleClick('https://gregoryjohncarter.github.io/weather-informer-index/', event); }}>Weather Informer Index <span className="fontOneA">&rarr; JS, Weather API</span></p>
           </div>
@@ -27,12 +27,12 @@ function Project() {
       </div>
       <div className="other-container">
         <div className="row">
-        <div className="col s12 m5 l6 colFlex">
+        <div className="col s12 l6 colFlex">
             <div className="secondary wo" onClick={(event) => { handleClick('https://powerful-beach-20504.herokuapp.com/', event); }} style={{cursor:"pointer"}}>
               <SocialIcon url='https://github.com/ryanweiler92/Workout-Assistant' className="iconHover" bgColor="#e0d4ec" fgColor="#2d2d2d" style={{ height: 45, width: 45}} target="_blank"/><p className="fontTwoA grating">Workout Assistant<span className="fontOneA">&rarr; MERN, Apollo Server</span></p>
             </div>
           </div>
-          <div className="col s12 m5 l6 colFlex">
+          <div className="col s12 l6 colFlex">
             <div className="secondary citizenship" onClick={(event) => { handleClick('https://boiling-fortress-60978.herokuapp.com/login', event); }} style={{cursor:"pointer"}}>
               <SocialIcon url='https://github.com/tcampbell01/Citizenship-community' className="iconHover" bgColor="#e0d4ec" fgColor="#2d2d2d" style={{ height: 45, width: 45}} target="_blank"/><p className="fontTwoA" onClick={(event) => { handleClick('https://boiling-fortress-60978.herokuapp.com/login', event); }}>Citizenship Community <span className="fontOneA">&rarr; Sequelize, Express</span></p>
             </div>
@@ -40,12 +40,12 @@ function Project() {
           
         </div>
         <div className="row">
-          <div className="col s12 m5 l6 colFlex">
+          <div className="col s12 l6 colFlex">
             <div className="secondary codeq" onClick={(event) => { handleClick('https://literatureleadsledger.herokuapp.com/', event); }} style={{cursor:"pointer"}}>
               <SocialIcon url='https://github.com/gregoryjohncarter/literature-leads-ledger' className="iconHover" bgColor="#e0d4ec" fgColor="#2d2d2d" style={{ height: 45, width: 45}} target="_blank"/><p className="fontTwoA">Literature Leads Ledger<span className="fontOneA">&rarr; MERN, Google Books API</span></p>
             </div>
           </div>
-          <div className="col s12 m5 l6 colFlex">
+          <div className="col s12 l6 colFlex">
             <div className="secondary thought" onClick={(event) => { handleClick('https://thought-bubble-boards.herokuapp.com/', event); }} style={{cursor:"pointer"}}>
               <SocialIcon url='https://github.com/gregoryjohncarter/thought-bubble-boards' className="iconHover" bgColor="#e0d4ec" fgColor="#2d2d2d" style={{ height: 45, width: 45}} target="_blank"/><p className="fontTwoA grating">Thought Bubble Boards<span className="fontOneA">&rarr; MySQL2, Express</span></p>
             </div>
