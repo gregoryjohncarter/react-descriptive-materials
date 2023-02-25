8e897c0a90b4065d7bcd03efa3be5df0329bb424
gregoryjohncarter <gregoryjohncarter@gmail.com>
2023-02-25 17:30:28 -0500

    misc. update

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Pdf from '../../assets/GJCResumeJan23.pdf'
+import Pdf from '../../assets/GJCResume.pdf'
 
 function Resume({ currentCategory }) {
   const { section, description, icon } = currentCategory;
